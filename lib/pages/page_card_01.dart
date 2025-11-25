3db9c1c1cec3fe5cd9ed49d96f5a961bb04a0d66
chaerulumammaulana <1123150132@global.ac.id>
2025-11-25 18:50:27 +0700

    mengganti height sizedbox untuk text chaerul

--- lib/pages/page_card_01.dart
+++ lib/pages/page_card_01.dart
@@ -16,40 +16,38 @@ class PageCard01 extends StatelessWidget {
       ),
 
       body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                SizedBox(height: 20),
-                Text("INI TEXT", style: TextStyle(fontSize: 16)),
-                SizedBox(
-                  height: 10
-                ),
-                SizedBox(height: 10),
-                Card(
-                  elevation: 8,
-                  margin: EdgeInsets.all(16.0),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      ListTile(
-                        leading: Icon(Icons.install_desktop_rounded, size: 50),
-                        title: Text('Belum Ada Judul'),
-                        subtitle: Text('Sebuah Judul Lagu Dari Iwan Fals'),
-                      ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              SizedBox(height: 20),
+              Text("INI TEXT", style: TextStyle(fontSize: 16)),
+              SizedBox(height: 10),
+              SizedBox(height: 10),
+              Card(
+                elevation: 8,
+                margin: EdgeInsets.all(16.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    ListTile(
+                      leading: Icon(Icons.install_desktop_rounded, size: 50),
+                      title: Text('Belum Ada Judul'),
+                      subtitle: Text('Sebuah Judul Lagu Dari Iwan Fals'),
+                    ),
                     Padding(
                       padding: EdgeInsetsGeometry.all(16.0),
                       child: Text(
                         'Card dapat di isi berbagai widget seperti text,'
                         'image, button, dan lainnya.',
                       ),
-                    )
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-                      SizedBox(height: 25),
-             Center(
+              ),
+              SizedBox(height: 25),
+              Center(
                 child: Container(
                   width: 330,
                   height: 200,
@@ -57,14 +55,11 @@ class PageCard01 extends StatelessWidget {
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(25),
                     gradient: const LinearGradient(
-                      colors: [
-                        Color(0xff143e67),
-                        Color(0xff1e4c7f),
-                      ],
+                      colors: [Color(0xff143e67), Color(0xff1e4c7f)],
                       begin: Alignment.topLeft,
                       end: Alignment.bottomRight,
                     ),
-                      boxShadow: [
+                    boxShadow: [
                       BoxShadow(
                         color: Colors.black,
                         blurRadius: 12,
@@ -72,10 +67,9 @@ class PageCard01 extends StatelessWidget {
                       ),
                     ],
                   ),
-                    child: Column(
+                  child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                    
                       Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
@@ -83,8 +77,8 @@ class PageCard01 extends StatelessWidget {
                           Icon(Icons.wifi, color: Colors.white70),
                         ],
                       ),
-                     SizedBox(height: 18),
-                     Text(
+                      SizedBox(height: 18),
+                      Text(
                         "1122 3245 5678 2104",
                         style: TextStyle(
                           color: Colors.white,
@@ -93,13 +87,10 @@ class PageCard01 extends StatelessWidget {
                           fontWeight: FontWeight.w500,
                         ),
                       ),
-                    SizedBox(height: 15),
-                     Text(
+                      SizedBox(height: 20),
+                      Text(
                         "Chaerul Umam Maulana",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 16,
-                        ),
+                        style: TextStyle(color: Colors.white, fontSize: 16),
                       ),
                       Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -109,32 +100,42 @@ class PageCard01 extends StatelessWidget {
                             children: const [
                               Text(
                                 "Expiry Date",
-                                style:
-                                    TextStyle(color: Colors.white70, fontSize: 12),
+                                style: TextStyle(
+                                  color: Colors.white70,
+                                  fontSize: 12,
+                                ),
                               ),
                               SizedBox(height: 3),
                               Text(
                                 "21/2027",
-                                style: TextStyle(color: Colors.white, fontSize: 14),
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 14,
+                                ),
                               ),
-                    ],
-                    ),
-                      Column(
+                            ],
+                          ),
+                          Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: const [
                               Text(
                                 "CVV",
-                                style:
-                                    TextStyle(color: Colors.white70, fontSize: 12),
+                                style: TextStyle(
+                                  color: Colors.white70,
+                                  fontSize: 12,
+                                ),
                               ),
-                                SizedBox(height: 3),
+                              SizedBox(height: 3),
                               Text(
                                 "2104",
-                                style: TextStyle(color: Colors.white, fontSize: 14),
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 14,
+                                ),
                               ),
-                        ],
-                  ),
-                    Row(
+                            ],
+                          ),
+                          Row(
                             children: [
                               Container(
                                 width: 28,
@@ -144,7 +145,7 @@ class PageCard01 extends StatelessWidget {
                                   shape: BoxShape.circle,
                                 ),
                               ),
-                        Transform.translate(
+                              Transform.translate(
                                 offset: const Offset(-10, 0),
                                 child: Container(
                                   width: 28,
@@ -153,31 +154,25 @@ class PageCard01 extends StatelessWidget {
                                     color: Colors.orange,
                                     shape: BoxShape.circle,
                                   ),
-                                      ),
+                                ),
                               ),
+                            ],
+                          ),
+                          SizedBox(height: 5),
+                          Text(
+                            "MasterCard",
+                            style: TextStyle(color: Colors.white, fontSize: 13),
+                          ),
+                        ],
+                      ),
                     ],
+                  ),
                 ),
-                SizedBox(height: 5),
-              Text(
-      "Mastercard",
-      style: TextStyle(
-        color: Colors.white,
-        fontSize: 13,
-      ),
-    ),
-                    ],
-                ),
-                
-                    ],
-                    
-             ),
-             
-             ),
-             ),
-              ],
               ),
-            ),
+            ],
           ),
-        );
+        ),
+      ),
+    );
   }
 }
