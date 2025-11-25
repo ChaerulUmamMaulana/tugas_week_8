00e083b11c13ac943ce5261cc294542e11afc183
chaerulumammaulana <1123150132@global.ac.id>
2025-11-25 16:18:03 +0700

    create materialApp

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      title: "Latihan Card",
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+      ),
+    );
   }
 }
