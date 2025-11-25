d6449fd1a474a53f0e4d65639ded90bde5b0d1d7
chaerulumammaulana <1123150132@global.ac.id>
2025-11-25 16:54:26 +0700

    membuat sizedbox, card, margin, child column untuk card wtih listtile

--- lib/pages/page_card_01.dart
+++ lib/pages/page_card_01.dart
@@ -16,19 +16,31 @@ class PageCard01 extends StatelessWidget {
       ),
 
       body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-          children: [
-            SizedBox(height: 20),
-            Text("INI TEXT", style: TextStyle(fontSize: 16)),
-            SizedBox(
-              height: 10
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                SizedBox(height: 20),
+                Text("INI TEXT", style: TextStyle(fontSize: 16)),
+                SizedBox(
+                  height: 10
+                ),
+                SizedBox(height: 10),
+                Card(
+                  elevation: 8,
+                  margin: EdgeInsets.all(16.0),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      
+                    ],
+                  ),
+                )
+              ],
+              ),
             ),
-          ],
           ),
-        ),
-      ),
-    );
+        );
   }
 }
