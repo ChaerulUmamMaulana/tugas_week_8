a7e1b6933cacbc4bf478d9d0a435daa4377eb2c2
chaerulumammaulana <1123150132@global.ac.id>
2025-11-25 16:31:07 +0700

    create pagecard01

---
+++ lib/pages/page_card_01.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class PageCard01 extends StatelessWidget {
+  const PageCard01({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
